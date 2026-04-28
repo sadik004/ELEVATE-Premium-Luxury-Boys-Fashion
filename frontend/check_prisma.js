063174f50464835374e97181ae71d47cfb35c683
Md. Moynul Hasan Sadik <163621400+sadik004@users.noreply.github.com>
2026-04-28 23:20:03 +0600

    Delete frontend directory

--- frontend/check_prisma.js
+++
@@ -1,7 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-async function main() {
-  const users = await prisma.user.findMany();
-  console.log(JSON.stringify(users, null, 2));
-}
-main().finally(() => prisma.$disconnect());
